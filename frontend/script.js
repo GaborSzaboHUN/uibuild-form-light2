3139ebfeea924c5b9f9e8bee7294f3afc2646e1c
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2022-05-31 02:05:15 +0200

    only #root div

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,29 @@
 const sectionElement = function () {
-    return ``
+    return `
+    <form action="" class="form-container">
+
+        <h1>Visszajelzés</h1>
+        <input type="text" placeholder="Tárgy">
+        <textarea name="comment" id="comment" cols="30" rows="10" placeholder="Megjegyzés"></textarea>
+
+        <div class="form-group">
+            <div>
+                <input type="checkbox">
+            </div>
+            <p>Elolvastam és elfogadom az <a href="#">Adatkezelési Tájékoztatót.</a></p>
+        </div>
+        <div class="form-group">
+            <div>
+                <input type="checkbox">
+            </div>
+            <p>Szeretnék hírlevelet kapni.</a></p>
+        </div>
+
+        <button>MENTÉS</button>
+
+    </form>
+
+    `
 }
 
 const loadEvent = function () {
